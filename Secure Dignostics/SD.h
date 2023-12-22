414506eb185dc39bc35a94927bd7ee768b26a412
Ola mohamed <66176966+Ola-Mohamed@users.noreply.github.com>
2023-12-22 22:31:35 +0200

    Update SD.h

--- Secure Dignostics/SD.h
+++ Secure Dignostics/SD.h
@@ -16,37 +16,30 @@
 #include <stdlib.h>
 #include <util/delay.h>
 /********************************************************************************************************/
-/*
-char MainService  = 0x31;
-#define SubService1         0x01
-#define Routine             0xAA00
-#define PositiveResponce    0x7101AA00
-#define NegativeResponce    0x7F3135
-#define SecurityAccess      0x2701
-#define SecurityAccessReply 0x6701
-#define EncryptionService   0x2702
-#define Ok_Responce         0x6702
-#define N_Ok_Responce       0x7F2735
-
-*/
-#define SIZE_ARRAY 4
-#define Initail_i  0
-/********************************************************************************************************/
-typedef enum{
-
-	E_ok,
-	E_Not_ok
-
-}State_Type;
+/* Preprocessor */
+#define SIZE_ARRAY 4      /* Define the size of an array as 4 */
+#define Initail_i  0      /* Define Initial value for i as 0 */
 
 /********************************************************************************************************/
+/* Enum for states */
+typedef enum {
+    E_ok,               /* Enum state for OK */
+    E_Not_ok            /* Enum state for Not OK */
+} State_Type;
 
+/********************************************************************************************************/
+/* Function Prototypes */
+
+/* Initialization function */
 State_Type Initilazition();
 
+/* Main task function */
 State_Type Main_task();
 
+/* Convert ASCII hex to decimal */
 unsigned char asciiHexToDecimal(const char *hexString);
 
+/* Convert decimal to ASCII hex string */
 void decimalToAsciiHexString(unsigned char decimalValue, char *hexString);
 
 #endif /* SD_H_ */
